730716f9b41cd8dc2b8a333558f63ebaad7476ed
AdityaChoureyG <adityachourey8319@gmail.com>
2025-07-21 22:30:27 +0530

    simple string problem

---
+++ July_2025/21_DeletingCharToMakeFancyString.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        char prevch = '3';
+        int cnt = 0;
+        string ans;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == prevch){
+                cnt++;
+            }
+            else{
+                prevch = s[i];
+                cnt = 1;
+            }
+            if(cnt<3)  ans += s[i];
+        }
+
+        return ans;
+    }
+};
