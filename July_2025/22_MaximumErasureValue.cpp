d96b0957a44f43f6d83b6a9410961f47e3936f7e
AdityaChoureyG <adityachourey8319@gmail.com>
2025-07-22 12:25:46 +0530

    sliding window problem

---
+++ July_2025/22_MaximumErasureValue.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int freq[10002] = {0};
+        int n = nums.size();
+        int ans = 0, sum = 0;
+
+        int i=0, j=0;
+        while(j<n){
+            freq[nums[j]]++;
+            sum += nums[j];
+            while(freq[nums[j]] > 1){
+                sum -= nums[i];
+                freq[nums[i]]--;
+                i++;
+            }
+            // cout<<i<<' '<<j<<endl;
+            ans = max(sum, ans);
+            j++;
+        }
+
+        return ans;
+    }
+};
